692cdfffc45b7d78fe6847ea273e34ae82b12010
Your Name <you@example.com>
2025-08-14 12:25:58 +0800

    update

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -29,7 +29,7 @@ export default function Header({ user }: { user: any }) {
   return (
     <>
     <header
-    className={`px-4 py-3 flex items-center justify-between w-full transition-all duration-300 bg-[#efefef] ${
+    className={`px-4 py-3 flex items-center justify-between w-full transition-all duration-300  ${
       isFixed
         ? 'fixed top-0 left-0 right-0 bg-white shadow-md z-50 slide-in'
         : 'relative slide-out'
