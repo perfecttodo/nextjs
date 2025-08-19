0adc932c626c45f4918cd3d5c2493df0f3da2bf1
Your Name <you@example.com>
2025-08-19 18:17:15 +0800

    update

--- next.config.ts
+++ next.config.ts
@@ -2,28 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  async headers() {
-    return [
-      {
-        // Apply this header to all routes
-        source: '/(.*)',  // Adjust the regex if you want to target specific routes
-        headers: [
-          {
-            key: 'Access-Control-Allow-Origin',
-            value: '*', // Change '*' to your allowed origin
-          },
-          {
-            key: 'Access-Control-Allow-Methods',
-            value: 'GET,POST,OPTIONS,PUT', // Specify allowed methods
-          },
-          {
-            key: 'Access-Control-Allow-Headers',
-            value: 'Content-Type,Authorization', // Specify allowed headers
-          },
-        ],
-      },
-    ];
-  },
+
 };
 
 export default nextConfig;
