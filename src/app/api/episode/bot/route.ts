21f8bd4a3ae19885a68cdbe6b42a8d5b1b2f483b
Your Name <you@example.com>
2025-08-27 11:58:25 +0800

    update

--- src/app/api/episode/bot/route.ts
+++ src/app/api/episode/bot/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { prisma } from '@/lib/prisma';
 
-import { detectAudioFormat,getMimeTypeFromUrl,fetchJson } from '@/slib/server';
+import { detectAudioFormat, getMimeTypeFromUrl, fetchJson } from '@/slib/server';
 
 const jsonUrl = process.env.BOT_NEW_JSON_URL;
 const onwerId = process.env.BOT_OWNER_ID;
@@ -10,65 +10,75 @@ const albumId = process.env.BOT_ALBUM_ID;
 
 function getFirst(e: { externalVideoFiles: { url: string; fileSize?: number }[] }) {
   return e.externalVideoFiles.sort((a, b) => {
-    if (a.url.endsWith( '.mp4')) return -1; // Move .m3u8 to the front
-    if (b.url.endsWith( '.mp4')) return 1;  // Keep others in their original order
+    if (a.url.endsWith('.mp4')) return -1; // Move .m3u8 to the front
+    if (b.url.endsWith('.mp4')) return 1;  // Keep others in their original order
     return 0; // No change for other elements
   })[0]
 }
 
 
+async function getItems(jsonUrl: string) {
+  const data = await fetchJson(jsonUrl);
+
+  return data.subCards.map((e: any) => {
+    let file = getFirst(e);
+    const url = file.url;
+    const fileSize = file.fileSize || 0;
+    const duration = e.videoMetadata.playTime;
+
+    return {
+      title: e.title,
+      url,
+      fileSize,
+      duration
+    }
+  });
+
+}
 export async function GET(request: NextRequest) {
   try {
 
-
-    if (!jsonUrl || !onwerId||!albumId) {
-      console.error(jsonUrl,onwerId,albumId);
+    if (!jsonUrl || !onwerId || !albumId) {
+      console.error(jsonUrl, onwerId, albumId);
       return NextResponse.json({ error: 'Missing required environment variables' }, { status: 500 });
     }
 
-    const data = await fetchJson(jsonUrl);
+    const items = await getItems(jsonUrl);;
 
+    for (let i = 0; i < items.length; i++) {
+      let e = items[i];
 
-
-    const itmes = data.subCards;
-
-    for (let i = 0; i < itmes.length; i++) {
-      let e = itmes[i];
-      let file = getFirst(e);
-      const url = file.url;
-      const fileSize = file.fileSize || 0;
       const status = 'published';
-      let format = getMimeTypeFromUrl(url);
-      if(!format){
-        const detectFormat = await detectAudioFormat(url);
-        format= detectFormat.format;
-       
+      let format = getMimeTypeFromUrl(e.url);
+      if (!format) {
+        const detectFormat = await detectAudioFormat(e.url);
+        format = detectFormat.format;
+
       }
 
-      const duration =  e.videoMetadata.playTime;
 
       // Save to database
-      try{
+      try {
         const episode = await prisma.episode.create({
           data: {
             title: e.title.trim(),
             originalName: e.title.trim(),
-            blobUrl: url,
+            blobUrl: e.url,
             format,
-            fileSize,
+            fileSize:e.fileSize,
             status,
             ownerId: onwerId,
             description: null,
             language: null,
             originalWebsite: null,
-            duration: typeof duration === 'string' ? parseInt(duration) : duration,
+            duration: typeof e.duration === 'string' ? parseInt(e.duration) : e.duration,
             albumId: albumId || null,
           }
         });
-      }catch(err){
-        console.log('err',err)
+      } catch (err) {
+        console.log('err', err)
       }
-   
+
 
     }
 
